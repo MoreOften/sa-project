06de27217dc7509a55a8b523f0a966ebd62b7ccb
MoreOften <nonratchapat@gmail.com>
2025-10-27 20:56:24 +0700

    change Main.java, add MainApplication.java, add maven in pom, add FXRouter

---
+++ sa-project/src/main/java/ku/cs/saproject/MainApplication.java
@@ -0,0 +1,28 @@
+package ku.cs.saproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ku.cs.services.FXRouter;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXRouter.bind(this, stage, "CS211 Project", 1280, 720);
+        configRoutes();
+
+        FXRouter.goTo("login");
+
+    }
+
+    private void configRoutes() {
+        String viewPath;
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
